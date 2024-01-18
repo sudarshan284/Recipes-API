f78754bcb00cb4ae9019e6d908bfdb313bca229b
sudarshan284 <sudarshanthakur18001@gmail.com>
2024-01-19 00:06:36 +0530

    recipe data model

--- main.go
+++ main.go
@@ -1,7 +1,13 @@
 package main
 
 import "github.com/gin-gonic/gin"
-
+type Recipe string {
+	Name string `json: "name"`
+	Tags []string `json:"tags"`
+	Ingredients []string `json:"ingredients"`
+	Instructions []string `json:"instructions"`
+	PublishedAt time.Time `json:"publishedAt"`
+}
 func main() {
 	router := gin.Default()
 	router.Run()
